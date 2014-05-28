ab26dd14ed9d05bb7caa436c32ef781c8acf84b3
yulangie <yulangie@yahoo.de>
2014-05-28 14:55:32 +0100

    created treasearch.cpp

---
+++ treesearch.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+
+class Node {
+public:
+	Node(int key, int val):key(key), val(val){}
+	int key;
+	double val;
+	list<Node* > children;
+};
+
+void create_testtree(Node& root) {
+	root.key = 5;
+	root.val =3.7;
+	Node* child1 = new Node(7,8.3);
+	Node* child2 = new Node(8,5.2);
+	Node* child3 = new Node(9,3.4);
+
+	Node* child21 = new Node(15,-0.1);
+	child2->children.push_back(child21);
+
+	root.children.push_back(child1);
+	root.children.push_back(child2);
+	root.children.push_back(child3);
+}
+
+
+int main (){
+
+	return 0;
+}
