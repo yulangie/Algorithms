988bb202062fcea27f258d17ab393b567ffa65fb
yulangie <yulangie@yahoo.de>
2014-05-28 22:11:38 +0100

    added find_node

--- treesearch.cpp
+++ treesearch.cpp
@@ -14,7 +14,13 @@ public:
 };
 
 void print_node(Node* node) {
-	printf("key = %d and val = %3.4f \n",node->key,node->val);
+	if (node!=NULL) {
+		printf("key = %d and val = %3.4f \n",node->key,
+			node->val);
+	}
+	else {
+		printf("Node doesn't exist!\n");
+	}
 }
 
 void print_tree_df(Node* node) {
@@ -30,28 +36,44 @@ void print_tree_df(Node* node) {
 	}
 }
 
+void push_children(queue<Node* >& nodes, Node* node){
+	list<Node* >::const_iterator it;
+	for (it = node->children.begin(); 
+		it!= node->children.end(); ++it){
+		nodes.push(*it);
+	}
+}
+
 void print_tree_bf(Node* root){
 	queue<Node* > allnodes;
 	allnodes.push(root);
-	list<Node* >::const_iterator it;
-
+	
 	while(!allnodes.empty()){
 		Node* node = allnodes.front();
 		allnodes.pop();
-		
+
 		print_node(node);
-		
-		for (it = node->children.begin(); 
-			it!= node->children.end(); ++it){
-			allnodes.push(*it);
-		}
+		push_children(allnodes,node);
+
 	}
 }	
 
 
 Node* find_node(Node* root, int key){
+	queue<Node* > allnodes;
+	allnodes.push(root);
 
+	while(!allnodes.empty()){
+		Node* node = allnodes.front();
+		allnodes.pop();
+		if(node->key == key){
+			return node;
+		}
+		push_children(allnodes,node);
+	}
+	return NULL;
 }
+
 
 void create_testtree(Node& root) {
 	root.key = 5;
@@ -75,6 +97,8 @@ int main (){
 	print_node(&root);
 	print_tree_df(&root);
 	print_tree_bf(&root);
+	Node* node = find_node(&root, 10);
+	print_node(node);
 
 	return 0;
 }
